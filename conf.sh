0a4a21983f276cc55bbdcf0df987ea403f504ab2
Dulguun Lkh <dulguun.lkhagvabaatar@yacoub.de>
2023-06-29 12:43:10 +0200

    1st com

---
+++ conf.sh
@@ -0,0 +1,4 @@
+rm -r ./build/ ./pnet*
+cmake -B build -S p-net -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DUSE_SCHED_FIFO=ON
+cmake --build build --target install
+./build/pn_dev -i enp1s0f0
